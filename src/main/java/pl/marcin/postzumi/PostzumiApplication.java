d3c3b449de1b4fa867083164ea11e3b3c273a831
Marcin Prze <marprz26@gmail.com>
2021-03-14 01:58:41 +0100

    Init app

---
+++ src/main/java/pl/marcin/postzumi/PostzumiApplication.java
@@ -0,0 +1,13 @@
+package pl.marcin.postzumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostzumiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostzumiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/marcin/postzumi/PostzumiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcin.postzumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostzumiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
