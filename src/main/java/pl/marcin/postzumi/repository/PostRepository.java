6eb31b852df9cb9fb873ae88b967a7b96e0e4eaf
Marcin Prze <marprz26@gmail.com>
2021-03-15 02:01:46 +0100

    Add get all posts by title method

--- src/main/java/pl/marcin/postzumi/repository/PostRepository.java
+++ src/main/java/pl/marcin/postzumi/repository/PostRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.marcin.postzumi.model.Post;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends CrudRepository<Post, Long> {
-
+    List<Post> getAllByTitle(String title);
 }
